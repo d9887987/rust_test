73ddfcea4ad5614af449f011f0772e806b0d19d2
d9887987 <599907482@qq.com>
2022-06-22 23:26:01 +0800

    Initial commit

---
+++ src/chapter_there/basic_grammar.rs
@@ -0,0 +1,70 @@
+
+
+pub fn variable(){
+    let x=5;
+    println!("x:{}", x);
+    let mut y=6.75;
+    println!("y:{}", y);
+    y=3.14;
+    println!("y:{}", y);
+    let x=x+5;
+    println!("x+5:{}", x);
+    println!("Rust 变量默认不可变，使用mut关键字可以改变，最后变量隐藏，类型推断时Rust的特色");
+
+    let nums=[1,2,3,4,5];
+    for x in nums.iter() {
+        println!("数组:{}", x);
+    }
+    for x in nums {
+        println!("数组:{}", x);
+    }
+    let tup=(1,2.1,5);
+    println!("元组tup.0:{}", tup.0);
+
+}
+
+pub fn function(a :i32,b: i32)->i32{
+    return a+b
+}
+
+pub fn data_type(){
+
+}
+
+pub fn method(){
+
+}
+
+pub fn loop_control_flow(){
+    let mut x=1;
+    loop {
+        if x==10{
+            println!("{}", x);
+            break;
+        }
+        x+=1;
+    };
+}
+
+pub fn if_control_flow(i: i32){
+    if i>=5{
+        println!("hello");
+    }else {
+        println!("no hello");
+    }
+}
+
+pub fn for_control_flow(){
+    let nums=[1,2,3,4,5];
+    for x in nums.iter() {
+        println!("数组:{}", x);
+    }
+}
+
+pub fn while_control_flow(mut i:i32){
+    while i < 10 {
+        println!("{}", i);
+        i=i+1;
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
 mod user_info;
 mod book_info;
 mod chapter_two;
+mod chapter_there;
 
 use crate::book_info::info_mation;
 use crate::book_info::service_info;
 use crate::user_info::user;
 use crate::user_info::service_go;
 use crate::chapter_two::guess_games;
+use crate::chapter_there::basic_grammar;
 
 
 fn main() {
-    guess_games::guess_games_num();
+    //guess_games::guess_games_num();
+    basic_grammar::variable();
+    basic_grammar::loop_control_flow();
+    basic_grammar::function(1,3);
+    basic_grammar::for_control_flow();
+    basic_grammar::if_control_flow(25);
+    basic_grammar::while_control_flow(5);
+
 }
