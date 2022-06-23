f668804044b0029ea75c0c80bd678636c7f7a1f9
d9887987 <599907482@qq.com>
2022-06-23 22:40:20 +0800

    chapter 04

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,30 @@
 mod user_info;
 mod book_info;
-mod chapter_two;
-mod chapter_there;
+mod chapter_2;
+mod chapter_3;
+mod chapter_4;
 
 use crate::book_info::info_mation;
 use crate::book_info::service_info;
 use crate::user_info::user;
 use crate::user_info::service_go;
-use crate::chapter_two::guess_games;
-use crate::chapter_there::basic_grammar;
+use crate::chapter_2::guess_games;
+use crate::chapter_3::basic_grammar;
+use crate::chapter_4::ownership;
 
 
-fn main() {
+fn main()  {
     //guess_games::guess_games_num();
-    basic_grammar::variable();
-    basic_grammar::loop_control_flow();
-    basic_grammar::function(1,3);
-    basic_grammar::for_control_flow();
-    basic_grammar::if_control_flow(25);
-    basic_grammar::while_control_flow(5);
+    // basic_grammar::variable();
+    // basic_grammar::loop_control_flow();
+    // basic_grammar::function(1,3);
+    // basic_grammar::for_control_flow();
+    // basic_grammar::if_control_flow(25);
+    // basic_grammar::while_control_flow(5);
+    let i = ownership::ownership_eg(String::from("hello world"));
+    println!("{}", i);
+    let string = String::from("hello world");
+    let slice1 = ownership::slice(&string[..]);
+    println!("{}", slice1);
 
 }
