c733824e8fcc7fa147317badbb622be77c135cff
d9887987 <599907482@qq.com>
2022-07-01 17:38:10 +0800

    chapter string

--- src/chapter_5/enumeg.rs
+++ src/chapter_5/enumeg.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use crate::ownership::slice;
 
 pub enum Relation{
@@ -27,4 +28,26 @@ pub fn add01(a:i32,b:i32)->i32{
 
 pub fn str_add(s:String,s1:String)->String{
     s1
+}
+
+pub fn string01(){
+    let mut x="gave".to_string();
+    let y=&x;
+    let j=&x;
+    println!("{}", y);
+    println!("{}", x);
+    println!("{}", j);
+}
+
+pub fn hash(){
+    let mut hash = HashMap::new();
+    hash.insert("java".to_string(),1);
+    hash.insert("golang".to_string(),2);
+    hash.insert("python".to_string(),1);
+    hash.insert("rust".to_string(),3);
+
+    for x in hash.iter() {
+        println!("{:?}", x);
+    }
+    println!("{:?}", hash);
 }
