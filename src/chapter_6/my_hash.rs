510c2082be293081ad920c0fad2361e41d747dac
d9887987 <599907482@qq.com>
2022-07-16 18:33:37 +0800

    chapter lambada

--- src/chapter_6/my_hash.rs
+++ src/chapter_6/my_hash.rs
@@ -1,11 +1,21 @@
 use std::collections::HashMap;
 
-pub fn demo_hash(){
+pub fn demo_hash()  {
     let mut map = HashMap::new();
-    map.insert("java",1);
-    map.insert("golang",2);
-    map.insert("rust",3);
-    for (key, value) in map {
-        println!("key:{:?},value:{:?}", key,value);
+    map.insert("java", 1);
+    map.insert("golang", 2);
+    map.insert("rust", 3);
+    for (key, value) in &map {
+        println!("key:{:?},value:{:?}", key, value);
     }
+    for key in map.keys() {
+        println!("{}", key);
+    }
+
+    for value in map.values() {
+        println!("{}", value);
+    }
+
+    println!("map length:{:?}", map.len());
+    println!("map cap:{:?}", map.capacity());
 }
