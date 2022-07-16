c135e9fcf775574353818934467cf35db72264de
d9887987 <599907482@qq.com>
2022-07-16 17:01:40 +0800

    chapter hash

--- src/chapter_5/enumeg.rs
+++ src/chapter_5/enumeg.rs
@@ -51,3 +51,16 @@ pub fn hash(){
     }
     println!("{:?}", hash);
 }
+
+pub fn two_sum(nums: &Vec<i32>, target: i32) -> Vec<i32> {
+    let mut map = HashMap::with_capacity(nums.len());
+    for i in 0..nums.len() {
+        if let Some(k) = map.get(&(target-nums[i])) {
+            if *k!=i{
+                return vec![*k as i32,i as i32];
+            }
+        }
+        map.insert(nums[i],i);
+    }
+    panic!("not found");
+}
