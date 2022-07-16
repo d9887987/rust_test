4fc5ee2095b7780689ca620924456175006d4096
d9887987 <599907482@qq.com>
2022-07-17 00:50:03 +0800

    chapter_9 impl

--- src/chapter_9/traits.rs
+++ src/chapter_9/traits.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 enum Classification {
     Dog,
     Person,
@@ -5,9 +6,22 @@ enum Classification {
 }
 
 
-pub struct Animal {
-    class: Classification,
-    name: String,
+#[derive(Debug)]
+pub struct Dog {
+    pub class: Classification,
+    pub name: String,
+}
+
+#[derive(Debug)]
+pub struct Person {
+    pub class: Classification,
+    pub name: String,
+}
+
+#[derive(Debug)]
+pub struct Car {
+    pub class: Classification,
+    pub name: String,
 }
 
 
@@ -16,13 +30,32 @@ pub trait Animals {
     fn call(&self);
 }
 
-
-impl Animals for Animal {
+impl Animals for Dog {
     fn eat(&self) {
-        todo!()
+        println!("{:?}", self.class)
     }
 
     fn call(&self) {
-        todo!()
+        println!("{:?}", self.name)
+    }
+}
+
+impl Animals for Person {
+    fn eat(&self) {
+        println!("{:?}", self.class)
+    }
+
+    fn call(&self) {
+        println!("{:?}", self.name)
+    }
+}
+
+impl Animals for Car {
+    fn eat(&self) {
+        println!("{:?}", self.class)
+    }
+
+    fn call(&self) {
+        println!("{:?}", self.name)
     }
 }
