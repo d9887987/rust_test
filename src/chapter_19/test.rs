e4485b2bf6d7c19b4db84009a5aca2cd8ff31842
d9887987 <599907482@qq.com>
2022-08-19 18:37:38 +0800

    merge nums

--- src/chapter_19/test.rs
+++ src/chapter_19/test.rs
@@ -1,9 +1,29 @@
-
 #[cfg(test)]
-mod test{
+mod test {
+    #[test]
+    fn eq() {
+        assert_eq!(4, 2 * 2);
+    }
 
     #[test]
-    fn eq(){
-        assert_eq!(4,2*2);
+    fn te() {
+        assert_ne!(5, 4)
+    }
+
+
+    pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
+        let mut m = m as usize;
+        let mut n = n as usize;
+        let mut right = nums1.len();
+        while n > 0 {
+            right -= 1;
+            if m == 0 || nums1[m - 1] < nums2[n - 1] {
+                nums1[right] = nums2[n - 1];
+                if n > 0 { n -= 1 }
+            } else {
+                nums1.swap(m - 1, right);
+                if m > 0 { m -= 1 }
+            }
+        }
     }
 }
